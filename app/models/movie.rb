7973d93f0cf02d88519d7242d966d2cfed7e5ce5
exit <karthikeyan.subramanian@mallow-tech.com>
2025-06-17 17:42:26 +0530

    Commit Done for video url-17/06/2025

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,12 +2,33 @@ class Movie < ApplicationRecord
   belongs_to :user
   has_many :movie_ratings, dependent: :destroy
   has_many :reviews, dependent: :destroy
-  validates :name, presence: true, uniqueness: true
-  has_many :ratings,dependent: :destroy
   has_one_attached :avatar, dependent: :destroy
+
+  validates :name, presence: true, uniqueness: true
+
+  scope :movie_filter, ->(start_date = Date.yesterday, end_date = Date.today){where("released_at between ? and ?",start_date,end_date)}
+
+  before_save :normalize_trailer_url
+
   def group_by_rating
     movie_ratings.group(:rating).count
   end
-  scope :movie_filter, ->(start_date = Date.yesterday, end_date = Date.today){where("released_at between ? and ?",start_date,end_date)}
 
+  private
+
+  def normalize_trailer_url
+    return if trailer_url.blank?
+
+    uri = URI.parse(trailer_url)
+    video_id = nil
+
+    if uri.host&.include?("youtu.be")
+      video_id = uri.path.delete_prefix("/")
+    elsif uri.host&.include?("youtube.com") && uri.query.present?
+      params = CGI.parse(uri.query)
+      video_id = params["v"]&.first
+    end
+
+    self.trailer_url = "https://www.youtube.com/embed/#{video_id}" if video_id.present?
+  end
 end
